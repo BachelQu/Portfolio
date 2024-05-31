df9e2b9765c04ba4f41f6d4bafac922faf9c83b6
Quentin Bacheley <quentinbacheley@Ordinateur-portable-de-Quentin.local>
2024-05-31 17:41:31 +0200

    Ajout JS scroll menu

--- app.js
+++ app.js
@@ -9,3 +9,38 @@ const openMenu = () => {
     }
 
 }
+
+function copyToClipboard() {
+    // Le nom d'utilisateur Discord et le tag
+    var discordTag = "bacchus0582#1234";
+    // Créer un élément temporaire pour copier le texte
+    var tempInput = document.createElement("input");
+    tempInput.style.position = "absolute";
+    tempInput.style.left = "-1000px";
+    tempInput.style.top = "-1000px";
+    tempInput.value = discordTag;
+    document.body.appendChild(tempInput);
+    tempInput.select();
+    document.execCommand("copy");
+    document.body.removeChild(tempInput);
+    alert("Le nom d'utilisateur Discord a été copié dans le presse-papiers. Veuillez le coller dans Discord pour ajouter en ami.");
+}
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute ('id');
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active')
+                document.querySelector('header nav a [href*='  + id +  ']').classList.add('active');
+            })
+        }
+    });
+};
